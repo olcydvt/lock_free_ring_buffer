3c8b03cf0f4d9435b5e6fc07a03dcab1ba6e8949
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-02-27 10:40:37 +0300

    improvements

--- lock_free_ring_buffer.hpp
+++ lock_free_ring_buffer.hpp
@@ -1,11 +1,9 @@
-#include <atomic>
-
-template<int size>
+template<int size, typename T = char>
 class ring_buffer {
     static constexpr int buffer_size = size;
-    char buffer[buffer_size];
-    std::atomic<size_t> read_cursor;
-    std::atomic<size_t> write_cursor;
+    T buffer[buffer_size];
+    alignas(64) std::atomic<size_t> read_cursor;
+    alignas(64) std::atomic<size_t> write_cursor;
 
 public:
 
@@ -17,22 +15,25 @@ public:
 
   
 
-    bool write(void* data, size_t size) {
+    bool write(const T* data, size_t size) {
         size_t old_write_cursor, new_write_cursor;
         do {
-            old_write_cursor = write_cursor.load(std::memory_order_acquire);
+            old_write_cursor = write_cursor.load(std::memory_order_relaxed);
+            size_t current_read_cursor = read_cursor.load(std::memory_order_acquire);
             new_write_cursor = (old_write_cursor + size) % buffer_size;
-            size_t available_space = buffer_size - (write_cursor - read_cursor);
-            if (new_write_cursor == read_cursor.load(std::memory_order_acquire) && size < available_space) {
+            size_t available_space = buffer_size - (old_write_cursor - current_read_cursor);
+            if (new_write_cursor + 1  == current_read_cursor || size < available_space) {
                 return false; // empty buffer
             }
         } while (!write_cursor.compare_exchange_weak(old_write_cursor, new_write_cursor, std::memory_order_release));
-        std::memcpy(buffer + old_write_cursor, data, size);
+        for (size_t i = 0; i < size; ++i) {
+            buffer[(old_write_cursor + i) & (buffer_size - 1)] = data[i];
+        }
         std::atomic_thread_fence(std::memory_order_release);  // Ensure memcpy completes before reading
         return true;
     }
 
-    bool read(void* data, size_t size) {
+    bool read(T* data, size_t size) {
         size_t old_read_cursor, new_read_cursor;
         do {
             old_read_cursor = read_cursor.load(std::memory_order_acquire);
@@ -43,7 +44,9 @@ public:
         } while (!read_cursor.compare_exchange_weak(old_read_cursor, new_read_cursor, std::memory_order_release));
 
         std::atomic_thread_fence(std::memory_order_acquire);  // Ensure memcpy completes before reading
-        std::memcpy(data, buffer + old_read_cursor, size);
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = buffer[(old_read_cursor + i) & (buffer_size - 1)];
+        }
         return true;
     }
 };
