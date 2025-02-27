af09a41b20ea82677086d73c7a53de66df0f3005
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-02-27 12:46:12 +0300

    still requires extra implementation for write and read

--- lock_free_ring_buffer.hpp
+++ lock_free_ring_buffer.hpp
@@ -1,9 +1,13 @@
+#include <iostream>
+#include <atomic>
+
 template<int size, typename T = char>
 class ring_buffer {
     static constexpr int buffer_size = size;
     T buffer[buffer_size];
     alignas(64) std::atomic<size_t> read_cursor;
     alignas(64) std::atomic<size_t> write_cursor;
+    std::atomic<T*> write_pointer;
 
 public:
 
@@ -11,6 +15,7 @@ public:
         memset(buffer, 0, buffer_size);
         read_cursor.store(0);
         write_cursor.store(0);
+        write_pointer.store(nullptr);
     }
 
   
@@ -26,10 +31,17 @@ public:
                 return false; // empty buffer
             }
         } while (!write_cursor.compare_exchange_weak(old_write_cursor, new_write_cursor, std::memory_order_release));
+    
+
         for (size_t i = 0; i < size; ++i) {
             buffer[(old_write_cursor + i) & (buffer_size - 1)] = data[i];
         }
-        std::atomic_thread_fence(std::memory_order_release);  // Ensure memcpy completes before reading
+
+        T* current_write_pointer;
+        do {
+            current_write_pointer = write_pointer.load(std::memory_order_acquire);
+        } while (!write_pointer.compare_exchange_weak(current_write_pointer, buffer + ((&buffer[buffer_size-1] - current_write_pointer)  + size) % buffer_size, std::memory_order_release));
+        
         return true;
     }
 
@@ -43,10 +55,31 @@ public:
             new_read_cursor = (old_read_cursor + size) % buffer_size;
         } while (!read_cursor.compare_exchange_weak(old_read_cursor, new_read_cursor, std::memory_order_release));
 
-        std::atomic_thread_fence(std::memory_order_acquire);  // Ensure memcpy completes before reading
+
+        
         for (size_t i = 0; i < size; ++i) {
             data[i] = buffer[(old_read_cursor + i) & (buffer_size - 1)];
         }
         return true;
     }
 };
+
+struct test {
+    int a;
+};
+
+int main()
+{
+    test t{};
+    t.a = 5;
+    ring_buffer<10, test> ring_bfr;
+    t.a = 3;
+    ring_bfr.write(&t, sizeof(t));
+
+    test* read_data{nullptr};
+    test* t_ptr = reinterpret_cast<test*>(ring_bfr.read(read_data, 4));
+
+    return 0;
+
+}
+
