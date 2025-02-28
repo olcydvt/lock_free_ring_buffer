4240f09f116f33f37da03a89756a2f0d241040b5
Olcay Davut Cabbas <olcay.d.cabbas@gmail.com>
2025-02-28 15:35:39 +0300

    some improvements

--- lock_free_ring_buffer.hpp
+++ lock_free_ring_buffer.hpp
@@ -1,50 +1,52 @@
 #include <atomic>
 
-template<int size, typename T = char>
+template<uint32_t buf_size, typename T = std::byte>
 class ring_buffer {
-    static constexpr int buffer_size = size;
+    static constexpr int buffer_size = buf_size;
     T buffer[buffer_size];
-    alignas(64) std::atomic<size_t> read_cursor;
-    alignas(64) std::atomic<size_t> write_cursor;
+    alignas(64) std::atomic<uint32_t> read_cursor;
+    alignas(64) std::atomic<uint32_t> write_cursor;
     std::atomic<T*> write_pointer;
     std::atomic<T*> read_pointer;
 
 public:
-
     ring_buffer() {
         memset(buffer, 0, buffer_size);
         read_cursor.store(0);
         write_cursor.store(0);
         write_pointer.store(nullptr);
         read_pointer.store(nullptr);
-    }  
+    }
 
-    bool write(const T* data, size_t size) {
-        size_t old_write_cursor, new_write_cursor;
+    T* get_buffer() {
+        return buffer;
+    }
+
+    [[nodiscard]] bool try_write(const T& data, uint32_t size) {
+        uint32_t old_write_cursor, new_write_cursor;
         do {
-            old_write_cursor = write_cursor.load(std::memory_order_relaxed);
-            size_t current_read_cursor = read_cursor.load(std::memory_order_acquire);
+            old_write_cursor = write_cursor.load(std::memory_order_acquire);
+            uint32_t current_read_cursor = read_cursor.load(std::memory_order_acquire);
             new_write_cursor = (old_write_cursor + size) % buffer_size;
-            size_t available_space = buffer_size - (old_write_cursor - current_read_cursor);
-            if (new_write_cursor + 1  == current_read_cursor || size > available_space) {
+            uint32_t available_space = buffer_size - (old_write_cursor - current_read_cursor);
+            if (new_write_cursor + 1 == current_read_cursor || size > available_space) {
                 return false; // empty buffer
             }
         } while (!write_cursor.compare_exchange_weak(old_write_cursor, new_write_cursor, std::memory_order_release));
-    
-        for (size_t i = 0; i < size; ++i) {
-            buffer[(old_write_cursor + i) & (buffer_size - 1)] = data[i];
-        }
+
+        uint32_t idx = old_write_cursor & (buffer_size - 1);
+        buffer[idx] = data;
 
         T* current_write_pointer;
         do {
             current_write_pointer = write_pointer.load();
         } while (!write_pointer.compare_exchange_weak(current_write_pointer, buffer + new_write_cursor, std::memory_order_release));
-        
+
         return true;
     }
 
-    bool read(T& data) {
-        size_t old_read_cursor, new_read_cursor;
+    [[nodiscard]] bool try_read(T& data) {
+        uint32_t old_read_cursor, new_read_cursor;
         do {
             old_read_cursor = read_cursor.load(std::memory_order_acquire);
             if (old_read_cursor == write_cursor.load(std::memory_order_acquire)) {
@@ -52,7 +54,7 @@ public:
             }
             new_read_cursor = (old_read_cursor + 1) % buffer_size;
         } while (!read_cursor.compare_exchange_weak(old_read_cursor, new_read_cursor, std::memory_order_release));
-       
+
         std::memcpy(&data, buffer + old_read_cursor, 1);
         return true;
     }
